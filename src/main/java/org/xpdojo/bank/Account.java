08ba8964779ae588715374c899a221c8982d4565
Alex Jironkin <alexjironkin@bmlltech.com>
2020-07-16 15:13:42 +0100

    Deposits can be made using deposit()

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,8 +1,15 @@
 package org.xpdojo.bank;
 
 public class Account {
+    private int balance = 0;
 
     public int balance() {
-        return 0;
+        /* Balance of the account */
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        /* Deposit an amount to the account */
+        balance += amount;
     }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -22,16 +22,26 @@ public class AccountTest {
         assertThat(account.balance()).isEqualTo(0);
     }
 
+    @Test
     public void singleDeposit() {
-
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
     }
 
+    @Test
     public void multipleDeposits() {
-
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(20);
     }
 
+    @Test
     public void singleNegativeDeposit() {
-
+        Account account = new Account();
+        account.deposit(-10);
+        assertThat(account.balance()).isEqualTo(-10);
     }
 
 }
