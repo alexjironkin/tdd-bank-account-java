97a0122c3f7c2d61a0bb608d42f4c8d908b2fb6c
Alex Jironkin <alexjironkin@bmlltech.com>
2020-07-16 15:27:35 +0100

    Refactored tests into modules

---
+++ src/test/java/org/xpdojo/bank/AccountDepositTest.java
@@ -0,0 +1,29 @@
+package org.xpdojo.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AccountDepositTest {
+    @Test
+    public void singleDeposit() {
+        Account account = new Account();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
+    }
+
+    @Test
+    public void multipleDeposits() {
+        Account account = new Account();
+        account.deposit(10);
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(20);
+    }
+
+    @Test
+    public void singleNegativeDeposit() {
+        Account account = new Account();
+        account.deposit(-10);
+        assertThat(account.balance()).isEqualTo(0);
+    }
+}
