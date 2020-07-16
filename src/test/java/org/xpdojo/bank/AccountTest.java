8094ead416b98e19fd7303f67fc2c716712ef9dd
Alex Jironkin <alexjironkin@bmlltech.com>
2020-07-16 15:00:02 +0100

    Tests for requirements

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,11 +4,34 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/*
+ * I can Deposit money to accounts
+ * I can Withdraw money from accounts
+ * I can Transfer amounts between accounts (if I have the funds)
+ * I can print out an Account balance slip (date, time, balance)
+ * I can print a statement of account activity (statement)
+ * I can apply Statement filters (include just deposits, withdrawal, date)
+ */
+
+
 public class AccountTest {
 
     @Test
-    public void depositAnAmountToIncreaseTheBalance() {
+    public void balanceIsZero() {
         Account account = new Account();
         assertThat(account.balance()).isEqualTo(0);
     }
+
+    public void singleDeposit() {
+
+    }
+
+    public void multipleDeposits() {
+
+    }
+
+    public void singleNegativeDeposit() {
+
+    }
+
 }
