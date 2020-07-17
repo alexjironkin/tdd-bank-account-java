cf146dd44033719e2d6382d439374b82ab7614ad
Alex Jironkin <alexjironkin@bmlltech.com>
2020-07-17 21:28:13 +0100

    Refactor tests to use starting balance

--- src/test/java/org/xpdojo/bank/AccountWithdrawlTest.java
+++ src/test/java/org/xpdojo/bank/AccountWithdrawlTest.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.Test;
 public class AccountWithdrawlTest {
     @Test
     public void singleWithdraw() {
-        Account account = new Account();
-        account.deposit(20);
+        Account account = new Account(20);
         account.withdraw(10);
 
         assertThat(account.balance()).isEqualTo(10);
@@ -17,16 +16,14 @@ public class AccountWithdrawlTest {
 
     @Test
     public void notEnoughFunds() {
-        Account account = new Account();
-        account.deposit(10);
+        Account account = new Account(10);
         account.withdraw(11);
         assertThat(account.balance()).isEqualTo(10);
     }
 
     @Test
     public void withdrawNegativeAmount() {
-        Account account = new Account();
-        account.deposit(10);
+        Account account = new Account(10);
         account.withdraw(-1);
         assertThat(account.balance()).isEqualTo(10);
     }
